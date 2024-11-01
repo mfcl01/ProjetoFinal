07eedb6515daad61aea3bb90ee88ca500652dc82
mfcl01 <manuelfcl@al.insper.edu.br>
2024-10-31 21:45:08 -0300

    Window nova

---
+++ Atravesse a rua.py
@@ -0,0 +1,24 @@
+import pygame
+from config import LARGURA, ALTURA, INIT, GAME, QUIT
+from init_screen import init_screen
+from game_screen import game_screen
+
+
+pygame.init()
+pygame.mixer.init()
+
+# ----- Gera tela principal
+window = pygame.display.set_mode((LARGURA, ALTURA))
+pygame.display.set_caption('Atravesse a rua!')
+
+state = INIT
+while state != QUIT:
+    if state == INIT:
+        state = init_screen(window)
+    elif state == GAME:
+        state = game_screen(window)
+    else:
+        state = QUIT
+
+# ===== Finalização =====
+pygame.quit()  # Função do PyGame que finaliza os recursos utilizados
--- init_screen.py
+++ init_screen.py
@@ -6,29 +6,42 @@ clock = pygame.time.Clock()
 # ----- Gera tela principal
 pygame.display.set_caption('Atravesse a Rua!')
 window = pygame.display.set_mode((1000, 595))
-
-image = pygame.image.load('assets\imagens\Captura de tela 2024-10-30 184033.png')
-
+image = pygame.image.load("assets folder/imagens/Captura de tela 2024-10-30 184033.png")
 image = pygame.image.load('Captura de tela 2024-10-30 184033.png')
-
 window.blit(image, (0, 0))
 pygame.mixer.music.load('musica lobby.mp3')
 pygame.mixer.music.play(-1)
+pygame.display.update() 
 # ----- Inicia estruturas de dados
 colour = (255, 0, 0)
 lobby = True
-game = True
+running = True
 # ===== Loop principal =====
-while game:
+while running:
 
     while lobby:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                lobby = False
-                game = False
+                running = False
             # ----- Verifica consequências
             if event.type == pygame.KEYDOWN:
+                    play = True
                     lobby = False
+                    break
+    window.fill(colour)
+    pygame.display.update() 
+    while play:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                play = False
+                running = False
+            if event.type == pygame.KEYDOWN:
+                play = False
+                running = False
+            
+            
+    
+                    
             # ----- Atualiza estado do jogo
         pygame.display.update()  # Mostra o novo frame para o jogador
         pygame.display.flip()
